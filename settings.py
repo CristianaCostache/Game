279b41be8d1d291996d1cec0ad73ef2f2577c029
Cristiana Costache <cristianagabriela99@gmail.com>
2024-02-01 21:33:41 +0200

    Creating the player

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 level_map = [
  '                  ',
- '                  ',
+ '      P            ',
  ' xx  xxx     xx   ',
  '                  ',
  '                  ',
@@ -14,3 +14,6 @@ level_map = [
 tile_size = 64
 screen_width = 1200
 screen_height = len(level_map) * tile_size
+speed = 8
+left_limit = screen_width // 4
+right_limit = screen_width - (screen_width // 4)
