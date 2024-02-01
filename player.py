4c771edcf297939ddde4bbf858c10efbac1f8d1d
Cristiana Costache <cristianagabriela99@gmail.com>
2024-02-01 22:00:11 +0200

    Making the player move and collide

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from settings import speed
+from settings import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
@@ -7,8 +7,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft=pos)
+
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = speed
+        self.gravity = gravity
+        self.jump_speed = jump_speed
+
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -20,6 +24,15 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
+
     def update(self):
         self.get_input()
-        self.rect.x += self.direction.x * self.speed
--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ level_map = [
  ' xx  xxx     xx   ',
  '                  ',
  '                  ',
- '                  ',
- '                  ',
- '                  ',
+ '       xxx        ',
+ '       xxx        ',
+ '  xxxxx           ',
  '                  ',
  '                  ',
  '                  ']
@@ -17,3 +17,5 @@ screen_height = len(level_map) * tile_size
 speed = 8
 left_limit = screen_width // 4
 right_limit = screen_width - (screen_width // 4)
+gravity = 0.8
+jump_speed = -16
